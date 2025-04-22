961bcadf372c03c590c7b22304a87e5fa0410232
HARI PRASAD L S <06hariumaraja@gmail.com>
2025-04-22 13:12:03 +0530

    Add files via upload
    C--Programming Variables and literals

---
+++ 2. Variables and literals in C programming/main.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+ {
+     int a=10;
+     char b='$';
+     float c=12.5f;
+     long int d=1418l;
+     int aa=0x41;
+     float bb=253e-2f;
+
+     printf("%d", a);
+     printf("\n");
+     /* THIS COMMAND PRINT THE VALUE */
+
+     printf("%d",b);
+     printf("\n");
+     /* THIS COMMAND PRINT THE SYSBOL VALUE */
+
+     printf("%c",b);
+     printf("\n");
+     /* THIS COMMAND PRINT THE SYSBOL */
+
+     printf("%0.2f",c);
+     printf("\n");
+     /* THIS COMMAND PRINT THE DECIMAL VALUE*/
+
+     printf("%0.3f",bb);
+     printf("\n");
+     /* THIS COMMAND PRINT THE DECIMAL POINT VALUE*/
+
+     return 0;
+ }
