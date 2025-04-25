0ecc7e0ca270349499ff52aa3d28e45bcfbfc4e0
HARI PRASAD L S <06hariumaraja@gmail.com>
2025-04-25 10:10:13 +0530

    Add files via upload
    /*
    operator       Example        Same as
       =             a=b           a=b
      +=            a+=b           a=a+b
      -=            a-=b           a=a-b
      *=            a*=b           a=a*b
      /=            a/=b           a=a/b
      %=            a%=b           a=a%b
    */

---
+++ 19.Assignment Operators/main.c
@@ -0,0 +1,9 @@
+/*
+operator       Example        Same as
+   =             a=b           a=b
+  +=            a+=b           a=a+b
+  -=            a-=b           a=a-b
+  *=            a*=b           a=a*b
+  /=            a/=b           a=a/b
+  %=            a%=b           a=a%b
+*/
