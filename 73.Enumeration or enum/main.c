57c79b618d18e4d4e940e554a3f6dd6e9efdc4d1
HARI PRASAD.L. S <06hariumaraja@gmail.com>
2025-07-11 05:47:45 +0530

    Enumeration or Enum
    Enumeration (or enum) is a user-defined data type that consists of a set of named integer constants.

---
+++ 73.Enumeration or enum/main.c
@@ -0,0 +1,14 @@
+//Enumeration or enum
+#include<stdio.h>
+enum bool{No=20,Yes=40};
+int main()
+{
+    enum point {x=18,y};
+    enum bool a;
+    enum bool b;
+    a=Yes;
+    b=y;
+    printf("\nA value : %d",a);
+    printf("\nB value : %d",b);
+    return 0;
+}
